3e3d0df8eed9570ae6837af2477144323f5a842d
MClarkTurner <mclarktu@fandm.edu>
2019-09-22 22:09:38 -0400

    comment out matplotlib

--- itr-evaluation/itr_evaluator.py
+++ itr-evaluation/itr_evaluator.py
@@ -2,6 +2,8 @@ import argparse
 import numpy as np
 import tensorflow as tf
 
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser(description="Ensemble model processor")
